368fd6f5e7b2643eb07b7156118cec3597fe5760
Yasmine Sharoda <yasminemmagdi@gmail.com>
2020-09-18 10:20:10 -0400

    making more use of Doc and less of string + prints assoc in one line

--- src/Tog/Exporting/agda.hs
+++ src/Tog/Exporting/agda.hs
@@ -7,6 +7,8 @@ import Tog.Deriving.TUtils (getArgExpr)
 
 import Control.Lens ((^.))
 import Text.PrettyPrint.Leijen
+import GHC.IO.Handle
+import System.IO
 
 class PrintAgda a where
   printAgda :: a -> Doc 
@@ -16,7 +18,7 @@ instance PrintAgda Name where
     text (nm ^. name)
 
 instance PrintAgda QName where
-  printAgda (Qual qn n) = printAgda qn <> text "." <> printAgda n
+  printAgda (Qual qn n) = printAgda qn <> dot <> printAgda n
   printAgda (NotQual n) = printAgda n 
 
 instance PrintAgda Constr where 
@@ -28,7 +30,7 @@ instance PrintAgda Arg where
 
 instance PrintAgda Binding where
   printAgda b =
-    let arguments as = encloseSep empty empty (text " ") (map printAgda as)
+    let arguments as = foldr (<+>) empty (map printAgda as)
         binding x =  arguments (getBindingArgs x) <+> text ofType <+> printAgda (getBindingExpr x) 
     in case b of
       Bind  _ _ -> parens $ binding b
@@ -36,25 +38,24 @@ instance PrintAgda Binding where
 
 instance PrintAgda Telescope where
   printAgda (Tel binds) =
-    encloseSep empty empty (text $ " " ++ pi_representation ++ " ") $ map printAgda binds
+    encloseSep empty empty (text pi_representation) $ map printAgda binds
 
 instance PrintAgda Expr where
   printAgda (Lam names expr) =
-    text lambda <//>
-    encloseSep empty empty (text " ") (map printAgda names) <//>
-    text lambdaArrow <//>
+    text lambda <+>
+    foldr (<+>) empty (map printAgda names) <+> 
+    text lambdaArrow <+>
     printAgda expr
   printAgda (Pi tel expr) =
-    printAgda tel <//> text (pi_representation) <//> printAgda expr
+    printAgda tel <+> text (pi_representation) <+> printAgda expr
   printAgda (Fun e1 e2) =
-    printAgda e1 <//> text fun_sep <//> printAgda e2
+    printAgda e1 <+> text fun_sep <+> printAgda e2
   printAgda (Eq e1 e2) = -- might need to have a bracket here 
     printAgda e1 <+> text equality_symbol <+> printAgda e2
   printAgda (App args) =
-    let pr = encloseSep empty empty (text " ") (map printAgda args)
+    let pr = foldr (<+>) empty (map printAgda args)
     in if length args == 1 then pr else parens pr     
   printAgda (Id qname) = printAgda qname 
-    
   
 
 -- the config that needs to be imported --
