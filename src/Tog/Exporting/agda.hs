5fca2112ad56cd7f2cdace003528779223326154
Yasmine Sharoda <yasminemmagdi@gmail.com>
2020-09-23 12:37:37 -0400

    opening the record type representing the theory in the printer, instead of the tog AST

--- src/Tog/Exporting/agda.hs
+++ src/Tog/Exporting/agda.hs
@@ -8,12 +8,12 @@ import Tog.Deriving.Utils.Bindings (getBindingArgs, getBindingExpr)
 import Tog.Deriving.TUtils (getArgExpr, getArgName, getName) 
 
 import Control.Lens ((^.))
-import Tog.Deriving.Lenses (name)
 import Text.PrettyPrint.Leijen as PP 
 import Data.Generics hiding (Constr, empty) 
 
+-- from out experience, it is useful to have a boolean variable needed in different places for different purposes. In some places it may not be needed, so it can be ignored 
 class PrintAgda a where
-  printAgda :: a -> Doc 
+  printAgda ::  a -> Doc 
 
 instance PrintAgda Name where
   printAgda nm =
@@ -21,35 +21,35 @@ instance PrintAgda Name where
 
 instance PrintAgda QName where
   printAgda (Qual qn n) = printAgda qn <> dot <> printAgda n
-  printAgda (NotQual n) = printAgda n 
+  printAgda (NotQual n) = printAgda n
 
 instance PrintAgda Constr where 
-  printAgda (Constr nm typ) =
+  printAgda (Constr nm typ)  =
     printAgda nm <+> text ofType <+> printAgda typ
 
 instance PrintAgda Arg where
-  printAgda arg = printAgda (getArgExpr arg) 
+  printAgda arg = printAgda (getArgExpr arg)
 
 instance PrintAgda Binding where
-  printAgda b =
-    let arguments as = foldr (<+>) empty (map printAgda as)
+  printAgda binds =
+    let arguments as = foldr (<+>) empty $ map (printAgda) as
         binding x =  arguments (getBindingArgs x) <+> text ofType <+> printAgda (getBindingExpr x) 
-    in case b of
-      Bind  _ _ -> parens $ binding b
-      HBind _ _ -> braces $ binding b 
+    in case binds of
+      Bind  _ _ -> parens $ binding binds
+      HBind _ _ -> braces $ binding binds
 
 hasPi :: [Expr] -> Bool 
 hasPi [] = False 
-hasPi ((Pi _ _):xs) = True
+hasPi ((Pi _ _):_) = True
 hasPi (_:xs) = hasPi xs 
 
 instance PrintAgda [Binding] where
   printAgda bindsList =
-   foldr (<+>) empty $ map printAgda bindsList 
-   -- encloseSep empty empty (text pi_representation) $ map printAgda bindsList
+   foldr (<+>) empty $ map (printAgda) bindsList 
+   -- encloseSep empty empty (text pi_representation) $ map printAgdaindsList
 
 instance PrintAgda [Constr] where
-  printAgda constrsList = vsep $ map printAgda constrsList   
+  printAgda constrsList = vsep $ map (printAgda) constrsList   
 
 instance PrintAgda Telescope where
   printAgda (Tel binds) = printAgda binds 
@@ -58,7 +58,7 @@ instance PrintAgda Expr where
   printAgda (Lam names expr) =
     parens $
      text lambda <+>
-     foldr (<+>) empty (map printAgda names) <+> 
+     foldr (<+>) empty (map (printAgda) names) <+> 
      text lambdaArrow <+>
      printAgda expr
   printAgda (Pi tel expr) =
@@ -70,13 +70,13 @@ instance PrintAgda Expr where
  -- printAgda (App (Arg ((App [ident,qual]):xs))) = -- the case of a qualified function name 
   printAgda (App args) =
     let (App newargs) = callLookup (App args) 
-        pr = foldr (<+>) empty (map printAgda $ newargs)
+        pr = foldr (<+>) empty $ map (printAgda) newargs
     in if length args == 1 then pr else parens pr     
   printAgda (Id qname) = printAgda qname
 
 instance PrintAgda Fields where
   printAgda NoFields = empty
-  printAgda (Fields constrs) = printAgda constrs 
+  printAgda (Fields cs) = printAgda cs 
 
 instance PrintAgda Params where
   printAgda (ParamDecl binds) = printAgda binds
@@ -84,16 +84,16 @@ instance PrintAgda Params where
 
 -- instead of sending the typ to recordHeader, we send the fields to check for the universe. 
 instance PrintAgda RecordBody where
-  printAgda (RecordDecl typ) = recordHeader Nothing -- typ 
-  printAgda (RecordDef typ flds) = recordHeader (Just flds) PP.<$> recordFields flds 
-  printAgda (RecordDeclDef typ constructor flds) =
+  printAgda (RecordDecl _) = recordHeader Nothing  
+  printAgda (RecordDef _ flds) = recordHeader (Just flds) PP.<$> recordFields flds 
+  printAgda (RecordDeclDef _ constructor flds) =
     recordHeader (Just flds) PP.<$> recordConstructor constructor PP.<$> recordFields flds
 
 instance PrintAgda DataBody where
   printAgda (DataDecl nm) = dataHeader nm 
-  printAgda (DataDef constrs) = printAgda constrs
-  printAgda (DataDeclDef nm constrs) =
-    dataHeader nm PP.<$> (indent 2 $ printAgda constrs) 
+  printAgda (DataDef cs) = printAgda cs
+  printAgda (DataDeclDef nm cs) =
+    dataHeader nm PP.<$> (indent 2 $ printAgda cs) 
 
 instance PrintAgda TypeSig where
   printAgda (Sig nm e) =
@@ -109,18 +109,18 @@ instance PrintAgda FunDefBody where
 
 instance PrintAgda Pattern where
   printAgda (EmptyP _) = empty
-  printAgda (ConP qnm ptrns) = parens $ printAgda qnm <+> (foldr (<+>) empty $ map printAgda ptrns)
+  printAgda (ConP qnm ptrns) = parens $ printAgda qnm <+> (foldr (<+>) empty $ map (printAgda) ptrns)
   printAgda (IdP qnm) = printAgda qnm
   printAgda (HideP ptrn) = braces $ printAgda ptrn 
 
 instance PrintAgda Import where
   printAgda (ImportNoArgs qn) = printAgda qn
-  printAgda (ImportArgs qn args) = printAgda qn <+> (foldr (<+>) empty $ map printAgda args) 
+  printAgda (ImportArgs qn args) = printAgda qn <+> (foldr (<+>) empty $ map (printAgda) args) 
 
 instance PrintAgda Decl where
   printAgda (TypeSig typ) = printAgda typ
   printAgda (FunDef nm patterns body) =
-    printAgda nm <+> (foldr (<+>) empty $ map printAgda patterns) <+> text fundef <+> printAgda body 
+    printAgda nm <+> (foldr (<+>) empty $ map (printAgda) patterns) <+> text fundef <+> printAgda body 
   printAgda (Data nm params body) =
     (text type_keyword) <+> printAgda nm <+> printAgda params <+> printAgda body  
   printAgda (Record nm params body) =
@@ -129,7 +129,7 @@ instance PrintAgda Decl where
   printAgda (Open imp) = text open <+> printAgda imp
   printAgda (Import imp) = text import_ <+> printAgda imp
   printAgda (OpenImport imp) = text open_import <+> printAgda imp
-  printAgda (Module_ m) = printAgda m 
+  printAgda (Module_ m) = printAgda $ addOpenDecl m -- to open the first declaration, which represents the equational theory. 
   printAgda _ = empty
 
 universeLevel :: Fields -> Doc
@@ -149,6 +149,10 @@ instance PrintAgda Module where
     (indent 2 $ printAgda decls)
 
 -- Utils functions --
+addOpenDecl :: Module -> Module 
+addOpenDecl (Module nm params (Decl_ (x:xs))) = Module nm params (Decl_ (x : (Open $ NotQual nm) : xs))
+addOpenDecl m = m
+    
 emptyTel :: Telescope -> Bool
 emptyTel (Tel b) = null b 
 
@@ -169,11 +173,11 @@ recordFields (Fields []) = empty
 recordFields flds =
   indent 2 $ text record_beforeFlds <$$> (indent 2 $ printAgda flds)
 
-dataHeader ∷ Name → Doc
+dataHeader ∷ Name -> Doc
 dataHeader nm = text ofType <+> printAgda nm <+> text type_beforeDecls
 
 -- the config that needs to be imported --
-ofType, lambda, lambdaArrow, pi_representation, fun_sep, equality_symbol, record_keyword, record_beforeDecls, record_constructor, record_beforeFlds :: String 
+ofType, lambda, lambdaArrow, pi_representation, fun_sep, equality_symbol, record_keyword, record_beforeDecls, record_constructor, record_beforeFlds, equality_import, type_keyword,type_beforeDecls,fundef, open, open_import, import_, module_, module_beforeDecls :: String 
 ofType = ":"
 lambda = "λ"
 lambdaArrow = "→"
@@ -195,12 +199,15 @@ module_ = "module"
 module_beforeDecls = "where"   
 
 {- handling imports -}
-{-
-An import replaces one or more of the prelude functions and might have an effect on how the function is called, we handle this here 
--}
-
+--An import replaces one or more of the prelude functions and might have an effect on how the function is called, we handle this here 
 -- the input to the exporter is a list of decls that start with some prelude functions. Some systems might have imports to replace these functions. This would be specified as follows
 
+{- 
+data PreludeAction =
+    DoNotExport
+  | ExportAsIs 
+  | Replace Doc
+-} 
 includeJ, includeSubst, includeSym, includeCong, includeTrans, includeUnit, includeEmptyT, includeNat, includePred, includeIsZero, includeZeroNotSuc, includeLemma, includeSucInj, includeFin, includeVec, includeLookup:: Bool
 includeJ = False
 includeSubst = False
@@ -237,7 +244,7 @@ replaceLookup = Nothing -}
 -- from all the prelude, we only call lookup, Staged and Code.
 
 callLookup :: Expr -> Expr
-callLookup a@(App [nm,a1,a2,a3]) =
+callLookup a@(App [nm,_,a2,a3]) =
   if (getArgName nm == "lookup") then App [nm,a3,a2] else a
 callLookup e = e 
 
@@ -259,7 +266,7 @@ removeDecl declNm (d:ds) =
   in if (headNm == declNm) then removeDecl declNm ds else d : removeDecl declNm ds
 
 filterOneDecl :: Bool -> String -> [Decl] -> [Decl]
-filterOneDecl bool str ds = if bool then ds else removeDecl str ds
+filterOneDecl flag str ds = if flag then ds else removeDecl str ds
   
 filterDecls ::[Decl] -> [Decl]
 filterDecls ds = 
@@ -288,3 +295,4 @@ exportAgda (Module nm params (Decl_ decls)) =
    text module_beforeDecls PP.<$>
    (indent 2 $
      (vsep $ imports ++ map printAgda (filterDecls decls)))
+exportAgda _ = error "toplevel needs to be a module" 
