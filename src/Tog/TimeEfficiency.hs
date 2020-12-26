6c2b7c85469a92800770efe9a9e2bb963fb7668d
Yasmine Sharoda <yasminemmagdi@gmail.com>
2020-12-25 23:11:54 -0500

    measuring time effeciency

--- src/Tog/TimeEfficiency.hs
+++ src/Tog/TimeEfficiency.hs
@@ -3,6 +3,49 @@
 
 module Tog.TimeEfficiency where
 
+{- ---- using $! ------ 
+*Tog.TimeEfficiency> timeTest "../../Library/mathscheme.tog"
+ flattening time: 26.57 us
+ Generation time:9.44 us
+ Scope-Checking time:14.41 s
+ Exporting time:1.26 ms
+*Tog.TimeEfficiency> timeTest "../../Library/mathscheme.tog"
+ flattening time: 10.38 us
+ Generation time:7.08 us
+ Scope-Checking time:12.92 s
+ Exporting time:18.70 us
+*Tog.TimeEfficiency> timeTest "../../Library/mathscheme.tog"
+ flattening time: 9.65 us
+ Generation time:7.49 us
+ Scope-Checking time:12.98 s
+ Exporting time:7.64 us
+*Tog.TimeEfficiency> timeTest "../../Library/mathscheme.tog"
+ flattening time: 10.75 us
+ Generation time:6.72 us
+ Scope-Checking time:12.96 s
+ Exporting time:7.60 us
+
+-- --------- using $!! and implemented instances of 
+
+*Tog.TimeEfficiency> timeTest "../../Library/mathscheme.tog"
+ flattening time: 5.42 s
+ Generation time:3.26 s
+ Scope-Checking time:7.85 s
+ Exporting time:8.89 us
+*Tog.TimeEfficiency> timeTest "../../Library/mathscheme.tog"
+ flattening time: 4.87 s
+ Generation time:2.93 s
+ Scope-Checking time:6.44 s
+ Exporting time:7.75 us
+*Tog.TimeEfficiency> timeTest "../../Library/mathscheme.tog"
+ flattening time: 4.75 s
+ Generation time:3.09 s
+ Scope-Checking time:6.77 s
+ Exporting time:11.34 us
+
+-} 
+
+
 import Tog.Raw.Abs
 import Tog.Parse 
 import Tog.ScopeCheck
